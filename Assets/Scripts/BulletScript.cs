179ccb80f5d2d6ecdfa259120c6e7989877f41e9
Streyan <leplongeon.celine@gmail.com>
2016-11-25 16:06:43 -0500

    Player can fire & get hit by fire

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour
+{
+    public Vector2 speed;
+    Rigidbody2D rigidBody;
+
+    public float delay;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, delay);
+        rigidBody.velocity = speed;
+    }
+
+    void Update()
+    {
+        rigidBody.velocity = speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(gameObject);
+    }
+}
