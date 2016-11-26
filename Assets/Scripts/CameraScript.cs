da8ca4bde52863b2932d3d35a96b1642dca23bc7
Paul <papattyn@gmail.com>
2016-11-26 16:28:57 -0500

    Added earthquake powa !

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -6,6 +6,8 @@ public class CameraScript : MonoBehaviour
     public Transform target;
 
     private bool isShaking = false;
+    private bool growStronger = true;
+    private float scale = 0f;
 
     // Update is called once per frame
     void Update()
@@ -14,7 +16,15 @@ public class CameraScript : MonoBehaviour
 
         if(isShaking)
         {
-            float rnd1 = Random.Range(0f, 10f) * .5f, rnd2 = Random.Range(0f, 10f) * .5f;
+            if (growStronger)
+                scale += .005f;
+            else
+                scale -= .005f;
+
+            if (scale <= 0f)
+                isShaking = false;
+
+            float rnd1 = Random.Range(0f, 10f) * scale, rnd2 = Random.Range(0f, 10f) * scale;
             float noise1 = Mathf.PerlinNoise(rnd1, rnd1), noise2 = Mathf.PerlinNoise(rnd2, rnd2);
 
             newPosition = new Vector3(target.position.x + noise1, target.position.y + noise2, transform.position.z);
@@ -25,14 +35,15 @@ public class CameraScript : MonoBehaviour
 
     public void shake(float time = 1)
     {
+        scale = 0f;
         isShaking = true;
+        growStronger = true;
         StartCoroutine(stopShaking(time));
     }
 
     private IEnumerator stopShaking(float time)
     {
         yield return new WaitForSeconds(time);
-
-        isShaking = false;
+        growStronger = false;
     }
 }
