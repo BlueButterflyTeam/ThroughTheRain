59c0e94238930e8878d5880b28132a793ee42f29
Paul <papattyn@gmail.com>
2016-11-26 02:27:12 -0500

    Added death & respawn

---
+++ Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointScript : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "player")
+        {
+            GameObject.Find("RespawnPoint").transform.position = GameObject.Find("player").transform.position;
+        }
+    }
+}
