983db641335e9cab1f5562421816bbfb2cf5fcee
Paul <papattyn@gmail.com>
2016-11-26 20:56:55 -0500

    Added air power

---
+++ Assets/Scripts/BreakBarrier.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakBarrier : MonoBehaviour {
+
+    public void breakBarrier()
+    {
+        gameObject.transform.GetChild(0).GetComponent<BreakObjects>().breakObject();
+        gameObject.transform.GetChild(1).GetComponent<BreakObjects>().breakObject();
+    }
+}
---
+++ Assets/Scripts/BreathAnimation.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreathAnimation : MonoBehaviour {
+
+    private bool appearing = true;
+
+    private SpriteRenderer spriteRenderer;
+    private Color color;
+
+    private float speed = 0.02f;
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        color = spriteRenderer.color;
+        color.a = 0f;
+        spriteRenderer.color = color;
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (appearing)
+        {
+            color.a += speed;
+
+            if (color.a >= 1)
+            {
+                appearing = false;
+            }
+        }
+        else
+        {
+            color.a -= speed;
+
+            if(color.a <= 0)
+            {
+                appearing = true;
+            }
+        }
+
+        spriteRenderer.color = color;
+    }
+}
