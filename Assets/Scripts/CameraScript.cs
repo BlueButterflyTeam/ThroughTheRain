188f77352eb67c41eda99b2967541cef77b0333b
Paul <papattyn@gmail.com>
2016-11-27 01:11:58 -0500

    - Added Ravine & Volcano scene - PlayerController is now a base class

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -12,7 +12,7 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        Vector3 newPosition = new Vector3(target.position.x, target.position.y + 1, transform.position.z);
 
         if(isShaking)
         {
