bd5ceab610849c5ff48f7d99da2c8dca0e6130a1
Paul <papattyn@gmail.com>
2016-11-28 06:02:29 -0500

    Pause menu fully functionnal

--- Assets/Scripts/UI/VideoSettings.cs
+++ Assets/Scripts/UI/VideoSettings.cs
@@ -3,17 +3,6 @@ using System.Collections;
 
 public class VideoSettings : MonoBehaviour {
 
-    
-
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     public void changeResolution(int value)
     {
         switch(value)
