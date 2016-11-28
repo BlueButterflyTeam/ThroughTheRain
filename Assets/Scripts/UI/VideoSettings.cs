036bc6adec0f953f6f551e5b212cd29229cc5a78
Paul <papattyn@gmail.com>
2016-11-28 05:01:12 -0500

    FInished layouts

---
+++ Assets/Scripts/UI/VideoSettings.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VideoSettings : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changeResolution(int value)
+    {
+        switch(value)
+        {
+            case 0:
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
+                break;
+            case 1:
+                Screen.SetResolution(1280, 720, Screen.fullScreen);
+                break;
+            case 2:
+                Screen.SetResolution(640, 480, Screen.fullScreen);
+                break;
+            case 3:
+                Screen.SetResolution(480, 360, Screen.fullScreen);
+                break;
+        }
+    }
+
+    public void setFullscreen(bool enabled)
+    {
+        Screen.fullScreen = enabled;
+    }
+}
